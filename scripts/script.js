d4ede5a3eaa1c99167933deb2df08cad5037edcf
Joaquin Pastor <joaquinpastorsuiffet@gmail.com>
2022-07-11 15:31:00 -0300

    About me

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,8 @@ const about = document.getElementById("text");
 
 $(document).ready(function () {
   myModal.show();
-  about.innerHTML = "";
+  about.innerHTML =
+    "Soy Desarrollador Fornt-End, egresado de Coder House, y actualmente estoy cursando el primer ano de la carrea de Analista de Sisitemas en la Escual Da Vinci. Tambien soy entrenador de categorias Sub-14 y Sub-18 de hockey sobre cesped en el club C.A.S.A. de Padua. Me considero una persona creativa, perseverante. Debido a mi trababjo actual y a que tambien formo parte de un equipo de hockey, trabajo muy bien en equipo.";
 });
 
 const skills = document.getElementById("skillsBtn");
